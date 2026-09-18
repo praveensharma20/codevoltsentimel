514ce9221e48949c02669849f41885acc4d0a74d
Praveen kumar <141123883+praveensharma20@users.noreply.github.com>
2026-09-18 18:07:58 +0530

    remove incompatible Auth0 wrapper

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-1zpnk5j6q63nmpk8.us.auth0.com"
-      clientId="jgHJ4adzZppCCs5U21RsLfNArcQWiuUH"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <App />der>
   </React.StrictMode>,
 );
